d2521b70ad610a39a25b21e6fa4f614a53d576d6
sworduserno2 <sworduser.no2@gmail.com>
2017-01-10 01:12:42 +0700

    new commit

--- soulplus_v1/apps/core/api.py
+++ soulplus_v1/apps/core/api.py
@@ -180,7 +180,9 @@ def updatenotification(userpk,actionid):
     try:
         friends = Friend.objects.filter(userid=userpk)
         for friend in friends:
+            print "creating notification"
             Notification.objects.create(userid=friend.friendid,notifcationtype='Liked',friend_like_id=userpk,actionlikeid=actionpk)
+            print "success"
     except:
         print "no friend"      
          
